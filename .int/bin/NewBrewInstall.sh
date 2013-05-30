252e2d61b29367d9ea6da16c141c03ccb816a6be
wbs75 <wbs75@me.com>
2013-05-30 00:02:33 -0300

    Update NewBrewInstall.sh

--- .int/bin/NewBrewInstall.sh
+++ .int/bin/NewBrewInstall.sh
@@ -2,7 +2,7 @@
 
 sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
 
-ln -s /opt/X11 /usr/X11
+sudo ln -s /opt/X11 /usr/X11
 
 sudo chown -R `whoami` /usr/local
 
@@ -11,10 +11,13 @@ ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 brew install bash-completion git  
  
 cd /usr/local 
+
 find Cellar  
+
 ls -l bin    
  
 git config --global user.name wbs75
+
 git config --global user.email wbs75@me.com
  
 echo 'export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"' >> ~/.bash_profile  
@@ -35,30 +38,50 @@ brew install brew-pkg
  
 source ~/.bash_profile 
 
+brew install rbenv 
+
 brew install ruby-build
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile 
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 exec $SHELL -l
+
+rbenv install 2.0.3
 
 rbenv rehash
 
 rbenv install 1.9.3-p385
 
+rbenv rehash
+
 rbenv install 1.9.3-p0
 
-rbenv shell 1.9.3-p0
+rbenv shell   1.9.3-p0
 
-rbenv global 1.9.3-p0
+rbenv global  1.9.3-p0
 
 rbenv rehash
 
-gem install bundler
+gem install bundler --no-ri --no-rdoc
 
-gem install rubygems-update
+gem install librarian --no-ri --no-rdoc
 
-gem install nokogiri 
- 
+gem install puppet --no-ri --no-rdoc
+
+gem install puppet-lint --no-ri --no-rdoc
+
+gem install librarian-puppet --no-ri --no-rdoc
+
+gem install bundler --no-ri --no-rdoc
+
+gem install rubygems-update --no-ri --no-rdoc
+
+gem install nokogiri --no-ri --no-rdoc
+
+rbenv rehash
+
+brew cleanup
+
 brew doctor
